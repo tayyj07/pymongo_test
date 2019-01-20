13ba3a19f41d2e51d8da1b21289b0f4fb6d94637
tayyj <tayyj07@gmail.com>
2019-01-20 22:39:16 +0800

    updated main.py

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 import pymongo
+from pprint import pprint
 
-print(pymongo.version)
+user = 'tayyjadmin'
+password = '123qweASD'
+database = 'tayyj_test'
+connection_end = 'tayyjtest-shard-00-00-tlyzm.mongodb.net:27017,tayyjtest-shard-00-01-tlyzm.mongodb.net:27017,tayyjtest-shard-00-02-tlyzm.mongodb.net:27017/test?ssl=true&replicaSet=tayyjtest-shard-0&authSource=admin&retryWrites=true'
+
+client = pymongo.MongoClient("mongodb://{0}:{1}@{2}".format(user, password, connection_end))
+db = client[database]
+db_collection = db.testing
+
+pprint(client.list_database_names())
